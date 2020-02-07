30dd1e6a50e4fb0dbac46d6422894999bf3dccbd
Willis Aybar <willisaybar@gmail.com>
2020-02-07 01:00:32 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,9 +1,11 @@
-## code your solution here. 
-class Cat 
-  attr_accessor :name 
-  
+## code your solution here.
+class Cat
+  attr_accessor :name
+
   def meow
     puts "meow!"
   end
-  
+
 end
+
+gsdf
